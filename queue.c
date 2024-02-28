d2932a036d2472a093aee81ca2bb86a4bd0a9ca9
lintin528 <mickey2000528@gmail.com>
2024-02-28 21:19:51 +0800

    Implement q_free function
    first check if it is a NULL queue.
    use list_for_each_safe in list.h to traverse this queue then free
    each element.
    lastly, free the head of this queue.

--- queue.c
+++ queue.c
@@ -22,7 +22,19 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (head == NULL)
+        return;
+    struct list_head *node;
+    struct list_head *safe;
+
+    list_for_each_safe (node, safe, head) {
+        element_t *next_elem = list_entry(node, element_t, list);
+        q_release_element(next_elem);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
